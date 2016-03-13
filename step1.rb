cedad5074b1ea3073802db00198d9c34468bb3f8
Max Cantor <max@maxcantor.net>
2016-03-13 18:07:18 -0400

    Temporarily mess with step 1 for example purposes

--- step1.rb
+++ step1.rb
@@ -3,22 +3,22 @@ require 'socket'
 # STEP 1: Just listen for one request, serve it, and end.
 
 # Open the wormhole
-server = TCPServer.new('127.0.0.1', 9090)
+server = TCPServer.new('0.0.0.0', 9090)
 
 # Someone wants to talk to us!
-connection = server.accept()
+while connection = server.accept()
+  # Read one line from the new connection
+  request = connection.gets()
 
-# Read one line from the new connection
-request = connection.gets()
+  # Log the line in terminal for reference
+  puts request
 
-# Log the line in terminal for reference
-puts request
+  # Send a response
+  connection.print("HTTP/1.1 200 OK\r\n")
+  connection.print("Content-Type: text/html\r\n")
+  connection.print("\r\n")
+  connection.print("Thank you for requesting a webpage. Please enjoy this bold text: <strong>Hooray!</strong>.")
 
-# Send a response
-connection.print("HTTP/1.1 200 OK\r\n")
-connection.print("Content-Type: text/html\r\n")
-connection.print("\r\n")
-connection.print("Thank you for requesting a webpage. Please enjoy.")
-
-# Thanks guy! See ya later.
-connection.close()
+  # Thanks friend! See ya later.
+  connection.close()
+end
